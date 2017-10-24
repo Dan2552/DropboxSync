729c2d8c8d1e3c4ef41634d381d1ba4a2b12d70f
Daniel Inkpen <dan2552@gmail.com>
2017-10-24 21:40:17 +0100

    in progress

--- Example/Tests/Sync/SyncProcessTests.swift
+++ Example/Tests/Sync/SyncProcessTests.swift
@@ -6,15 +6,15 @@ class SyncProcessSpec: QuickSpec {
     var describedInstance: SyncProcess!
     var client: MockDropboxClient!
     var localCollection: SyncCollection!
-    
+
     var listFiles: ListFilesMock!
     var downloadFiles: DownloadFilesMock!
     var sync: SyncMock!
-    
+
     var completionHandler: SyncProcessCompletionHandler!
-    
+
     var completionHandlerResult: SyncProcessResult?
-    
+
     func setup() {
         // defaults
         client = client ?? MockDropboxClient()
@@ -22,44 +22,49 @@ class SyncProcessSpec: QuickSpec {
         completionHandler = completionHandler ?? { result in
             self.completionHandlerResult = result
         }
-        
+
         listFiles = listFiles ?? ListFilesMock(client: client)
         downloadFiles = downloadFiles ?? DownloadFilesMock(client: client)
         sync = sync ?? SyncMock()
 
-        describedInstance = SyncProcess(
+        describedInstance = describedInstance ?? SyncProcess(
             listFiles: listFiles,
             downloadFiles: downloadFiles,
             localCollection: SyncCollection(),
             sync: sync
         )
     }
-    
+
     override func spec() {
         beforeEach {
-            self.describedInstance = nil
-            self.localCollection = nil
             self.client = nil
+            self.localCollection = nil
+            self.completionHandler = nil
+            self.listFiles = nil
+            self.downloadFiles = nil
+            self.sync = nil
+            self.describedInstance = nil
         }
-        
+
         describe("#perform") {
             func subject() {
                 setup()
                 describedInstance.perform(completion: completionHandler)
             }
-            
+
             it("downloads a list of files from Dropbox") {
                 subject()
                 XCTAssert(self.listFiles.didFetch)
             }
-            
+
             it("downloads the meta files") {
                 subject()
                 XCTAssert(self.downloadFiles.didPerform)
             }
-            
+
             context("no metafiles (i.e. first sync for remote") {
                 beforeEach {
+                    self.setup()
                     self.downloadFiles.performReturn = []
                 }
 
@@ -67,19 +72,20 @@ class SyncProcessSpec: QuickSpec {
                     subject()
                     XCTAssert(self.sync.didSync)
                 }
-                
+
                 it("persists the sync status") {
-                    
+
                 }
-                
+
                 it("calls completion with .success") {
                     subject()
                     XCTAssert(self.completionHandlerResult == .success)
                 }
             }
-            
+
             context("the downloaded metafiles were readable") {
                 beforeEach {
+                    self.setup()
                     self.downloadFiles.performReturn = [
                         mockMetaFile()
                     ]
@@ -102,6 +108,7 @@ class SyncProcessSpec: QuickSpec {
 
             context("an unreadable metafile is downloaded") {
                 beforeEach {
+                    self.setup()
                     self.downloadFiles.performReturn = [
                         URL(string: "/invalid/url")!
                     ]
